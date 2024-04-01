9c4ecba3e22b5a385d37bb86ab259b41e032a59a
Charonus2012 <117681966+Charonus2012@users.noreply.github.com>
2024-04-01 08:42:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
   <body>
     <!-- page content -->
     <h3>Sign up</h3>
-    <form action="./store_example.inc.php" method="post">
+    <form action="./includes/store_example.inc.php" method="post">
         <input type="text" name="username" placeholder="Username"><br>
         <Button>Sign up</Button>
     </form>
     <h3>Show name of id</h3>
-    <form action="./get_example.inc.php" method="post">
+    <form action="./includes/get_example.inc.php" method="post">
         <input type="number" name="id" placeholder="id"><br>
         <Button>Show name of id</Button>
     </form>
